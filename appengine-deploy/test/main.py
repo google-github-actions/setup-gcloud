280442a25c8673bc06596605916d0a8a38c1106d
Seth Vargo <seth@sethvargo.com>
2020-06-08 11:34:17 -0400

    Inline entire app

--- appengine-deploy/test/main.py
+++ appengine-deploy/test/main.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START gae_python37_app]
 from flask import Flask
 
 
@@ -32,4 +31,3 @@ if __name__ == '__main__':
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python37_app]

10ad2a8775e80b7d409b43ffef7749b9e975c987
fcoclavero <francisco.clavero@thenotcompany.com>
2020-07-13 17:59:38 -0400

    ncc

--- get-iap-token/index.js
+++ get-iap-token/index.js
@@ -26,8 +26,8 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const webhookPayload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${webhookPayload}`);
 } catch (error) {
   core.setFailed(error.message);
-}̦
+}

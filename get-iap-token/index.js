3d1c143d9d95b829f4f494bbc9a759fb87f103df
fcoclavero <francisco.clavero@thenotcompany.com>
2020-07-14 15:58:45 -0400

    Parse credentials instead of receiving the secret. Added github actions.

--- get-iap-token/index.js
+++ get-iap-token/index.js
@@ -1,15 +1,33 @@
 const core = require('@actions/core');
 const jwt = require('jsonwebtoken');
 
+
+/** Parse the given credentials into an object.
+ *
+ * If the credentials are not JSON, they are probably base64-encoded. Even
+ * though we don't instruct users to provide base64-encoded credentials,
+ * sometimes they still do.
+ *
+ * @param {object} credentials - the credentials object. Either JSON or
+ *   base64-encoded JSON.
+ * @return {object} The parsed credentials object.
+ */
+function parseCredentials(credentials) {
+  if (!credentials.trim().startsWith('{')) {
+    credentials = Buffer.from(credentials, 'base64').toString('utf8');
+  }
+  return JSON.parse(credentials);
+}
+
+
 try {
   // Get the input defined in action metadata file
   const IAPOAuthClientID = core.getInput('iap-oauth-client-id');
-  const privateKey = core.getInput('private-key');
+  const privateKey = parseCredentials(core.getInput('credentials')).private_key;
   const serviceAccount = core.getInput('service-account');
   const tokenDuration = Number(core.getInput('token-duration'));
   const iat = Math.floor(Date.now() / 1000)
   const exp = iat + tokenDuration
-
   const payload = {
     'iss': serviceAccount,
     'aud': 'https://oauth2.googleapis.com/token',
